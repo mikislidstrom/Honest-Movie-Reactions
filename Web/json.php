d6aedf7e83e6325dd392051859da3e6735c7c897
Johan Nilsson <johan.nilsson@student.gu.se>
2014-11-05 15:59:32 +0100

    New changes to web front end

--- Web/json.php
+++ Web/json.php
@@ -1,6 +1,6 @@
 <?php	
         header("Content-type: application/json");
-
-        $json = file_get_contents('http://127.0.0.1:10018/buckets/Movies/keys/100');
+        $id = $_GET['id'];
+        $json = file_get_contents('http://127.0.0.1:10018/buckets/Movies/keys/'.$id);
         echo $json;
 ?>
---
+++ Web/moviekeys.php
@@ -0,0 +1,5 @@
+<?php	
+        header("Content-type: application/json");
+        $json = file_get_contents('http://127.0.0.1:10018/buckets/Movies/keys/?keys=true');
+        echo $json;
+?>
