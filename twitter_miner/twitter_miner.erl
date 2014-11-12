67c391a0b92da38d3a874eb52fb0c4d1e16d15d8
Mikaela <guslidstmi@student.gu.se>
2014-11-12 10:57:01 +0100

    Changed twitter miner to save the ID as well

--- twitter_miner/twitter_miner.erl
+++ twitter_miner/twitter_miner.erl
@@ -22,10 +22,10 @@ twitter_search(Movie) ->
 	URL = "https://api.twitter.com/1.1/search/tweets.json",
   Params = [{q, Movie}, {lang, en}],
 
-  Api_key = "XXXXX",
-  Api_secret = "XXXXX",
-  Access_token = "XXXXX",
-  Access_token_secret = "XXXXX",
+  Api_key = "QtSP14USgvF4Zj9IKHy2I5bKN",
+  Api_secret = "c7j35Y4vU7mw2K5vdm4oAwS6VLViLutd4ZcORCC8ByAJCBm1qV",
+  Access_token = "2827251826-bGt8aDBRHkySUiHW5XJBkOJ8u3vZFNS0gUhgHd5",
+  Access_token_secret = "Ktwz5o3bYYsLSBA8fpTOk78UFkCGNtGU9NVV7b0fwoP2M",
 
   Consumer = {Api_key, Api_secret, hmac_sha1},
   Token = Access_token,
@@ -48,8 +48,11 @@ twitter_search(Movie) ->
     %lists:map(fun unpack/1, A3).
 
   unpack(List) ->
+    {<<"id">>,  Id} = lists:keyfind(<<"id">>, 1, List),
     {<<"text">>, Text} = lists:keyfind(<<"text">>, 1, List),
     {<<"created_at">>, Date} = lists:keyfind(<<"created_at">>, 1, List),
     {<<"user">>, {Locked_List}} = lists:keyfind(<<"user">>, 1, List),
     {<<"screen_name">>, Screen_Name} = lists:keyfind(<<"screen_name">>, 1, Locked_List),
-    {Date, Screen_Name, Text}.
+    {Id, Date, Screen_Name, Text}.
+
+    
