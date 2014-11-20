e44f85ab526dc8dd2c3a901ea90f83fb79dc258f
Johan Nilsson <johan.nilsson@student.gu.se>
2014-11-20 14:24:04 +0100

    moved files to Server folder

---
+++ Server/test_db.erl
@@ -0,0 +1,43 @@
+-module(test_db).
+-export([store_movie/1, get_movie/1, store_movies/1, delete_movies/1, update_movies/0, id_title_list/0]).
+
+%% Stores movies in the database from a list of movie ids
+store_movies([]) -> ok;
+store_movies([X|Xs]) -> 
+	Obj = movies:id(X),
+	db_handler:put("Movies", X, Obj),
+	store_movies(Xs).
+
+%% Stores the movie in the database for the inputed movie id 
+store_movie(Id) -> 
+	Obj = movies:id(Id),
+	db_handler:put("Movies", Id, Obj).
+
+%% Gets the movie data from the database for a movie with inputed movie id
+get_movie(Id) ->
+	db_handler:get("Movies", Id).
+
+
+%% Deletes movies from the database from a list of movie ids
+delete_movies([]) -> ok;
+delete_movies([X|Xs]) ->
+	db_handler:delete("Movies", X),
+	delete_movies(Xs).
+
+%% Refreshes the data for all the movies in the database
+update_movies() ->
+	store_movies(db_handler:keys("Movies")).
+
+%% Helper function
+movielist(List) -> movielist(List, []).
+
+movielist([], List) -> List;
+movielist([{Movie}|MovieList], List) ->
+	movielist(MovieList, [{proplists:get_value(<<"id">>, Movie), proplists:get_value(<<"title">>, Movie)}|List]).
+
+
+%% Get id and title for all movies in db
+id_title_list() ->
+	Keys = db_handler:keys("Movies"),
+	MovieList =[jiffy:decode(get_movie(Id))||Id <- Keys],
+	movielist(MovieList).
---
+++ Server/twitter_miner.erl
@@ -0,0 +1,42 @@
+-module(twitter_miner).
+-export([get_account_keys/1, twitter_search/1, unpack/1]).
+
+twitter_search(Movie) ->
+	URL = "https://api.twitter.com/1.1/search/tweets.json",
+  Params = [{q, Movie}, {lang, en}],
+
+  Api_key = "xxx",
+  Api_secret = "xxx",
+  Access_token = "xxx-xxx",
+  Access_token_secret = "xxx",
+
+  Consumer = {Api_key, Api_secret, hmac_sha1},
+  Token = Access_token,
+  Secret = Access_token_secret,
+
+
+
+	ssl:start(),
+	application:start(inets),	
+  {ok, {{_, 200, _}, _, Body}} = oauth:get(URL, Params, Consumer, Token, Secret),
+  parse(Body).
+
+  parse(Body) ->
+    {A} = jiffy:decode(Body),
+    [A1|B] = A,
+    {Header, A2} = A1,
+    [{A3}|B2] = A2,
+
+    List = [twitter_miner:unpack(X)||{X}<-A2],
+    List.
+    %lists:map(fun unpack/1, A3).
+
+  unpack(List) ->
+    {<<"id">>,  Id} = lists:keyfind(<<"id">>, 1, List),
+    {<<"text">>, Text} = lists:keyfind(<<"text">>, 1, List),
+    {<<"created_at">>, Date} = lists:keyfind(<<"created_at">>, 1, List),
+    {<<"user">>, {Locked_List}} = lists:keyfind(<<"user">>, 1, List),
+    {<<"screen_name">>, Screen_Name} = lists:keyfind(<<"screen_name">>, 1, Locked_List),
+    {Id, Date, Screen_Name, Text}.
+
+    
