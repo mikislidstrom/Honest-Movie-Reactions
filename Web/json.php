0aecd51cf2b51fef477d7a71d4291773f0211328
Johan Nilsson <johan.nilsson@student.gu.se>
2014-11-20 14:44:12 +0100

    Removed php files

--- Web/json.php
+++
@@ -1,6 +0,0 @@
-<?php	
-        header("Content-type: application/json");
-        $id = $_GET['id'];
-        $json = file_get_contents('http://127.0.0.1:10018/buckets/Movies/keys/'.$id);
-        echo $json;
-?>
