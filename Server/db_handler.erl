5013fbbb7a2b6d1c7b8090d520ff759ade1198aa
Johan Nilsson <johan.nilsson@student.gu.se>
2014-11-24 10:34:33 +0100

    Added clause for putting tweets to db

--- Server/db_handler.erl
+++ Server/db_handler.erl
@@ -1,13 +1,19 @@
 -module(db_handler).
 -export([put/3, get/2, buckets/0, keys/1, keys_json/1, delete/2]).
 
--define(PORT, 8087).
+-define(PORT, 10017).
 -define(HOST, "127.0.0.1").
 
 %%% Module works as a wrapper for the riak-erlang-client 
 %%% accessing and writing data to the database
 
 % Put an object with a key in a bucket
+put(Bucket, Key, Object) when is_binary(Object) -> 
+	{ok, Pid} = riakc_pb_socket:start_link(?HOST, ?PORT), %% IP needs to be changed
+	RiakObject = riakc_obj:new(list_to_binary(Bucket), list_to_binary(Key), Object, <<"application/json">>),
+	riakc_pb_socket:put(Pid, RiakObject),
+	riakc_pb_socket:stop(Pid);
+
 put(Bucket, Key, Object) ->
 	{ok, Pid} = riakc_pb_socket:start_link(?HOST, ?PORT), %% IP needs to be changed
 	RiakObject = riakc_obj:new(list_to_binary(Bucket), list_to_binary(Key), list_to_binary(Object), <<"application/json">>),
