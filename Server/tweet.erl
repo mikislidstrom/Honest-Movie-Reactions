db22eb4763389a0e91ea4a00925420023d605015
Edholm92 <gusedhhe@student.gu.se>
2014-11-25 11:36:50 +0100

    Added comments to the module.

--- Server/tweet.erl
+++ Server/tweet.erl
@@ -1,10 +1,18 @@
+%% @author: Henrik Edholm
+%% @doc: This module get a tweet from the server, 
+%%		 starts a python process that makes calculations on the tweet 
+%%		 and finally returns the value to the server.
+
 -module(tweet).
 -export([twitterator/1, python_start/1, flush/0]).
 
+%% Get a tweet from the server, put it in a list and send to next function.
 twitterator(Binary) ->
 	List = string:tokens(binary_to_list(Binary), "\r\n\t "),
     python_start(List).
 
+%% Starts a python process that calculates the sentiment value of the tweet
+%% and returns it to the server.
 python_start(List) ->
 	{ok, P} = python:start(),
 	python:call(P, tweet, register_tweet, [self()]),
