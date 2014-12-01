4a481eb9e1b86ab647fa047d816b2565937f7fad
Mikaela <guslidstmi@student.gu.se>
2014-12-01 15:57:52 +0100

    removed binary to list

--- Server/tweet.erl
+++ Server/tweet.erl
@@ -8,12 +8,12 @@
 
 %% Get a tweet from the server, put it in a list and send to next function.
 twitterator(MovieTitle, Binary) ->
-	Title = string:tokens(string:to_lower(binary_to_list(MovieTitle)), "\r\n\t "),
+	Title = string:tokens(string:to_lower(MovieTitle), "\r\n\t "),
 	NewList = string:tokens(string:to_lower(binary_to_list(Binary)), "\r\n\t "),
 	python_start(remove_Title(Title, NewList)).
 
 remove_Title([], List) -> List;
-remove_Title([H|T], List) -> erlang:display(List), remove_Title(T, lists:delete(H, List)).
+remove_Title([H|T], List) -> remove_Title(T, lists:delete(H, List)).
 
 %% Starts a python process that calculates the sentiment value of the tweet
 %% and returns it to the server.
