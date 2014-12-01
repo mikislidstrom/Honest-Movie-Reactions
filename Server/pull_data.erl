60c34a7666145e2dd73266d8ff6aa12c30c97a5f
elsawide <elsa.wide@outlook.com>
2014-12-01 20:28:45 +0100

    Get ratings

---
+++ Server/pull_data.erl
@@ -0,0 +1,35 @@
+-module(pull_data).
+-export([pull_data/1, pull_data2/1]).
+
+pull_data(Imdb_Id) ->
+	Short_Id = string:sub_string(Imdb_Id,3),
+	Api_Key = "?apikey=nuyfaesekthcgc45fd7umc4j",
+	Filter = "http://api.rottentomatoes.com/api/public/v1.0/movie_alias.json",
+	Id = "&type=imdb&id=" ++ Short_Id,
+	Url = Filter ++ Api_Key ++ Id,
+	inets:start(),
+	ssl:start(),
+	{ok, {{_, 200, _}, _, Body}} = httpc:request(get, {Url, []}, [], []),
+	Now_showing = mochijson2:decode(Body),
+	{struct,Firststep} = Now_showing,
+	{value, {<<"ratings">>, Secondstep}} = lists:keysearch(<<"ratings">>, 1, Firststep),
+	{struct,Thirdstep} = Secondstep,
+	{value, Fourthstep} = lists:keysearch(<<"critics_score">>, 1, Thirdstep),
+	Fourthstep.
+
+pull_data2(Imdb_Id) ->
+	First_Filter = "http://www.omdbapi.com/?i=",
+	Second_Filter = "&plot=short&r=json",
+	Url = First_Filter ++ Imdb_Id ++ Second_Filter,
+	inets:start(),
+	ssl:start(),
+	{ok, {{_, 200, _}, _, Body}} = httpc:request(get, {Url, []}, [], []),
+	Now_showing = mochijson2:decode(Body),
+	{struct,Firststep} = Now_showing,
+	{value, {<<"imdbRating">>, Secondstep}} = lists:keysearch(<<"imdbRating">>, 1, Firststep),
+	Thirdstep = binary_to_float(Secondstep),
+	Fourthstep = Thirdstep * 10,
+	{<<"imdbRating">>,Fourthstep}.
+
+
+
