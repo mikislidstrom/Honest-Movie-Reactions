fe99ecbb08d2a5502f1be138b2f0a76b8f6a7b6c
Mikaela <guslidstmi@student.gu.se>
2014-12-01 15:35:56 +0100

    removes title before handing the text over to python

--- Server/tweet.erl
+++ Server/tweet.erl
@@ -4,19 +4,23 @@
 %%		 and finally returns the value to the server.
 
 -module(tweet).
--export([twitterator/2, python_start/2, flush/0]).
+-export([twitterator/2, python_start/2, flush/0, remove_Title/2]).
 
 %% Get a tweet from the server, put it in a list and send to next function.
 twitterator(MovieTitle, Binary) ->
-	List = string:tokens(binary_to_list(Binary), "\r\n\t "),
-    python_start(MovieTitle, List).
+	Title = string:tokens(string:to_lower(binary_to_list(MovieTitle)), "\r\n\t "),
+	NewList = string:tokens(string:to_lower(binary_to_list(Binary)), "\r\n\t "),
+	python_start(remove_Title(Title, NewList)).
+
+remove_Title([], List) -> List;
+remove_Title([H|T], List) ->erlang:display(H), remove_Title(T, lists:delete(H, List)).
 
 %% Starts a python process that calculates the sentiment value of the tweet
 %% and returns it to the server.
-python_start(MovieTitle, List) ->
+python_start(List) ->
 	{ok, P} = python:start(),
 	python:call(P, tweet, register_tweet, [self()]),
-	python:cast(P, {MovieTitle, List}),
+	python:cast(P,  List),
 	Rating = flush(),
 	python:stop(P),
 	Rating.
