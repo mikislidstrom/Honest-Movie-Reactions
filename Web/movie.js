3411929155921838e608920688fab78e516eb447
Fortoul <fortoul_@hotmail.com>
2014-12-11 21:42:32 +0100

    frontend update

--- Web/movie.js
+++ Web/movie.js
@@ -4,6 +4,7 @@ var id = "101";
 var keys;
 var statistics;
 var highlightedMovie;
+var jsonArr;
 
 function updatePage() {
 
@@ -31,25 +32,33 @@ function updatePage() {
         'Total Revenue': '#0099cc',
         }
         });
+
+    if($.cookie('tutorial1') == undefined) {
+    alert("On this page you can view information and statistics about movies.\n\nUse the search-field together with the SEARCH-button to search for a specific movie by its title or click the RANDOM-button to check-out a random movie.\n\nYou can view more statistics about a movie by clicking on 'VIEW FULL STATISTICS', additional movies can be added in order to compare statistics by clicking 'ADD TO COMPARE'.");
+    var date = new Date();
+    var minutes = 15;
+    date.setTime(date.getTime() + (minutes * 60 * 1000));
+    $.cookie('tutorial1', 'true', { expires: date });
+    }
 }
 
 function updateStatPage() {
 
-$.cookie.json = true;
+$.cookie.json = true; 
+    
+    var movie = [$.cookie('movie1'), $.cookie('movie2'), $.cookie('movie3'), $.cookie('movie4'), $.cookie('movie5')];
 
-    var movie1 = $.cookie('movie1');
-    var movie2 = $.cookie('movie2');
-    var movie3 = $.cookie('movie3');
-    var movie4 = $.cookie('movie4');
-    var movie5 = $.cookie('movie5');
-
-    $("#vote_average").text("Average vote: " + movie1.vote_average);
-    $("body").css('background-image', 'url(' + 'https://image.tmdb.org/t/p/w780' + movie1.backdrop_path +')');
-    $("#thumbnail1").attr({"src": "https://image.tmdb.org/t/p/w500" + movie1.poster_path, "title":movie1.title});
+    $("#vote_average").text("Average vote: " + movie[0].vote_average);
+    $("body").css('background-image', 'url(' + 'https://image.tmdb.org/t/p/w780' + movie[0].backdrop_path +')');
+    $("#thumbnail1").attr({"src": "https://image.tmdb.org/t/p/w500" + movie[0].poster_path, "title":movie[0].title});
+    
+    if (highlightedMovie == null) {
     highlightedMovie = "thumbnail1";
+    }
+
     document.getElementById(highlightedMovie).style.border = "5px inset black";
 
-    var jsonArr = [{'Budget':movie1.budget, 'Total Revenue':movie1.revenue}];
+    jsonArr = [{'Budget':movie[0].budget, 'Total Revenue':movie[0].revenue}];
 
     chart1.load({    
         json: jsonArr,
@@ -60,7 +69,15 @@ $.cookie.json = true;
         'Budget': '#ffa500',
         'Total Revenue': '#0099cc',
         }
-        });
+        })
+
+    if($.cookie('tutorial2') == undefined) {
+    alert("On this page you can view more detailed statistics about a movie.\nYou may stack up to 5 movies on this page and compare their statistics against each other.\n\nThe movies you have selected to compare are displayed as thumbnails:\nHOVER a thumbnail to see the movie title.\nLEFT-CLICK a thumbnail to highlight a particular movie (this will also load it on the start page).\nRIGHT-CLICK a thumbnail to remove a particular movie from comparison.")
+    var date = new Date();
+    var minutes = 15;
+    date.setTime(date.getTime() + (minutes * 60 * 1000));
+    $.cookie('tutorial2', 'true', { expires: date });
+    }
 
     $('.thumbnail').mouseover(function(event) {
         switch (event.which) {
@@ -69,7 +86,7 @@ $.cookie.json = true;
                 document.getElementById(this.id).style.border = "5px inset black";
                 break;
            }
-    });
+    })
 
     $('.thumbnail').mouseout(function(event) {
         switch (event.which) {
@@ -78,7 +95,7 @@ $.cookie.json = true;
                 document.getElementById(highlightedMovie).style.border = "5px inset black";
                 break;
            }
-    });
+    })
 
     $('.thumbnail').mousedown(function(event) {
         switch (event.which) {
@@ -86,20 +103,81 @@ $.cookie.json = true;
                 document.getElementById(highlightedMovie).style.border = "none";
                 document.getElementById(this.id).style.border = "5px inset black";
                 highlightedMovie = this.id;
+                var number = highlightedMovie.charAt(9);
+                //var movie Has to be declared again inside this function due to timing to avoid refreshing entire script
+                var movie = [$.cookie('movie1'), $.cookie('movie2'), $.cookie('movie3'), $.cookie('movie4'), $.cookie('movie5')];
+                $("body").css('background-image', 'url(' + 'https://image.tmdb.org/t/p/w780' + movie[number-1].backdrop_path +')');
+
+                jsonArr = [{'Budget':movie[number-1].budget, 'Total Revenue':movie[number-1].revenue}];
+
+                chart1.load({    
+                    json: jsonArr,
+                    keys: {
+                        value: ['Budget', 'Total Revenue']
+                    },
+                    colors: {
+                    'Budget': '#ffa500',
+                    'Total Revenue': '#0099cc',
+                    }
+                    });
+
                 break;
+
             case 3:
-                remove();
+                var number = this.id.charAt(9);
+                $.cookie.json = true;
+
+                switch (amountOfMovies()) {
+                    case 1: 
+                    alert("Why would you want to do that?");
+                    updateStatPage();
+                    break;        
+
+                    case 2: 
+                    $.cookie('movie'+number, $.cookie('movie'+(parseInt(number)+1).toString()));
+                    $.removeCookie('movie2');
+                    document.getElementById("thumbnail2").style.visibility = "hidden";
+                    updateStatPage();
+                    break;
+
+                    case 3:
+                    $.cookie('movie'+number, $.cookie('movie'+(parseInt(number)+1).toString()));
+                    $.cookie('movie'+(parseInt(number)+1).toString(), $.cookie('movie'+(parseInt(number)+2).toString()));
+                    $.removeCookie('movie3');
+                    document.getElementById("thumbnail3").style.visibility = "hidden";
+                    updateStatPage();
+                    break;
+
+                    case 4: 
+                    $.cookie('movie'+number, $.cookie('movie'+(parseInt(number)+1).toString()));
+                    $.cookie('movie'+(parseInt(number)+1).toString(), $.cookie('movie'+(parseInt(number)+2).toString()));
+                    $.cookie('movie'+(parseInt(number)+2).toString(), $.cookie('movie'+(parseInt(number)+3).toString()));
+                    $.removeCookie('movie4');
+                    document.getElementById("thumbnail4").style.visibility = "hidden";
+                    updateStatPage();
+                    break;
+
+                    case 5: 
+                    $.cookie('movie'+number, $.cookie('movie'+(parseInt(number)+1).toString()));
+                    $.cookie('movie'+(parseInt(number)+1).toString(), $.cookie('movie'+(parseInt(number)+2).toString()));
+                    $.cookie('movie'+(parseInt(number)+2).toString(), $.cookie('movie'+(parseInt(number)+3).toString()));
+                    $.cookie('movie'+(parseInt(number)+3).toString(), $.cookie('movie'+(parseInt(number)+4).toString()));
+                    $.removeCookie('movie5');
+                    document.getElementById("thumbnail5").style.visibility = "hidden";
+                    updateStatPage();
+                    break;
+                }
                 break;
         }
-    });
+    })
 
-    $("#thumbnail2").attr({"src": "https://image.tmdb.org/t/p/w500" + movie2.poster_path, "title":movie2.title});
+    $("#thumbnail2").attr({"src": "https://image.tmdb.org/t/p/w500" + movie[1].poster_path, "title":movie[1].title});
     document.getElementById("thumbnail2").style.visibility = "visible";
-    $("#thumbnail3").attr({"src": "https://image.tmdb.org/t/p/w500" + movie3.poster_path, "title":movie3.title});
+    $("#thumbnail3").attr({"src": "https://image.tmdb.org/t/p/w500" + movie[2].poster_path, "title":movie[2].title});
     document.getElementById("thumbnail3").style.visibility = "visible";
-    $("#thumbnail4").attr({"src": "https://image.tmdb.org/t/p/w500" + movie4.poster_path, "title":movie4.title});
+    $("#thumbnail4").attr({"src": "https://image.tmdb.org/t/p/w500" + movie[3].poster_path, "title":movie[3].title});
     document.getElementById("thumbnail4").style.visibility = "visible";
-    $("#thumbnail5").attr({"src": "https://image.tmdb.org/t/p/w500" + movie5.poster_path, "title":movie5.title});
+    $("#thumbnail5").attr({"src": "https://image.tmdb.org/t/p/w500" + movie[4].poster_path, "title":movie[4].title});
     document.getElementById("thumbnail5").style.visibility = "visible";
 
 }
@@ -115,9 +193,6 @@ function random() {
         }
     });
 
-
-
-    /*var list = ["500","123","102651","157336","205587","106","97","120","116","254904","245891","262543","103","96","100","13","113","239563","207933","107","122","98566","114","112","125","603","111","115","124","98","121","105","101","95","240832","228150","104","118","117","204922", "137113", "184315", "91314", "225886", "241254", "189", "138103"];*/
     var r = Math.floor((Math.random() * keys.length));
 
 
@@ -194,34 +269,66 @@ function init() {
 function initStatPage() {
 
     $.cookie.json = true;
-        $.cookie('movie1', $.cookie('movieJSONcurr'));
+    $.cookie('movie1', $.cookie('movieJSONcurr'));
     statistics = window.open("statistics.html","statistics");
-    statistics.updateStatPage();
 
+}
+
+function amountOfMovies() {
+    $.cookie.json = true;
+    var amount;
+
+    if($.cookie('movie2') == undefined) {
+        amount = 1;
+    }
+
+    else if($.cookie('movie3') == undefined) {
+        amount = 2;
+    }
+
+    else if($.cookie('movie4') == undefined) {
+        amount = 3;
+    }
+
+    else if($.cookie('movie5') == undefined) {
+        amount = 4;
+    }
+
+    else {
+        amount = 5;
+    }  
+
+    return amount;
 }
 
 function add() {
+    $.cookie.json = true;
 
-$.cookie.json = true;
+    switch (amountOfMovies()) {
 
-if($.cookie('movie2') == undefined) {
-$.cookie('movie2', $.cookie('movieJSONcurr'));
-statistics.updateStatPage();
-}
+        case 1: 
+        $.cookie('movie2', $.cookie('movieJSONcurr'));
+        statistics.updateStatPage();
+        break;        
 
-else if($.cookie('movie3') == undefined) {
-$.cookie('movie3', $.cookie('movieJSONcurr'));
-statistics.updateStatPage();
-}
+        case 2: 
+        $.cookie('movie3', $.cookie('movieJSONcurr'));
+        statistics.updateStatPage();
+        break;
 
-else if($.cookie('movie4') == undefined) {
-$.cookie('movie4', $.cookie('movieJSONcurr'));
-statistics.updateStatPage();
-}
+        case 3: 
+        $.cookie('movie4', $.cookie('movieJSONcurr'));
+        statistics.updateStatPage();
+        break;
 
-else {
-$.cookie('movie5', $.cookie('movieJSONcurr'));
-statistics.updateStatPage();
-}   
+        case 4: 
+        $.cookie('movie5', $.cookie('movieJSONcurr'));
+        statistics.updateStatPage();
+        break;
+
+        case 5: 
+        alert("Easy there NERD! You are currently comparing 5 movies, remove at least one before any more can be added.");
+        break;
+    }  
 
 };
